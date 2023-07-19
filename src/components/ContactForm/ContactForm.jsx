aba3b93c5064a107d87bbce1c43162622d5f73b4
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-19 03:44:04 +0300

    Add Formik-Yup using

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,67 +1,85 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactForm.module.css';
 import { FcAddDatabase } from 'react-icons/fc';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { object, string } from 'yup';
 
-export class ContactForm extends Component {
-  state = {
+//Регулярні вирази для валідації відповідних полів форми
+const regexName = "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
+// const regexNumber =
+//   '+?d{1,4}?[ .-s]?(?d{1,3}?)?[ .-s]?d{1,4}[ .-s]?d{1,4}[ .-s]?d{1,9}';
+
+//Схема для валідації полів форми
+const schema = object({
+  name: string()
+    .matches(regexName, 'Not valid')
+    .min(2, 'Name too short')
+    .max(15, 'Name too short')
+    .trim()
+    .required('Required'),
+  number: string()
+    // .matches(regexNumber, 'Not valid')
+    .min(5, 'Number too short')
+    .max(15, 'Number too long')
+    .trim()
+    .required('Required'),
+});
+
+export const ContactForm = ({ onSubmit }) => {
+  const initialValues = {
     name: '',
     number: '',
   };
-  // Метод оновлення полів інпутів
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
-  };
-  // Метод сабміту форми
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    onSubmit(values);
+    resetForm();
   };
 
-  //Метод очищення полів форми
-  resetForm = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    return (
-      <form className={css.form_wrapper} onSubmit={this.handleSubmit}>
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      <Form className={css.form_wrapper}>
         <FcAddDatabase size={'35px'} className={css.icon} />
         <label className={css.label}>
           Name
-          <input
+          <Field
             className={css.input}
-            type="text"
             name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+          />
+          <ErrorMessage
+            component="div"
+            className={css.error_name}
+            name="name"
           />
         </label>
         <label className={css.label}>
           Number
-          <input
+          <Field
             className={css.input}
-            type="tel"
             name="number"
-            value={this.state.number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            // pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
           />
         </label>
+        <ErrorMessage
+          component="div"
+          className={css.error_number}
+          name="number"
+        />
         <button className={css.button_add} type="submit">
           Add contact
         </button>
-      </form>
-    );
-  }
-}
+      </Form>
+    </Formik>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
